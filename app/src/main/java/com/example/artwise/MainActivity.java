f09b49cde61b9cecf7dc6a10fc2680782356e875
samu7173 <samuelmatas1997@gmail.com>
2023-03-26 01:09:20 +0100

    Lectura

--- app/src/main/java/com/example/artwise/MainActivity.java
+++ app/src/main/java/com/example/artwise/MainActivity.java
@@ -1,23 +1,109 @@
 package com.example.artwise;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanRecord;
+import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variable de la interfaz
+    private Button scanButton;//boton comenzar
+    private Button repetirButton;//boton Repetir
+    private Button masInfo;
+    private Button velocidad;
+    private ArrayList<String> leidos = new ArrayList<String>();
+    private boolean scanning;
+
+    private String text;
+    private String titulo;
+    private String descripcion;
+    private String disString;
+    private int conversionRSSI;
+
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle drawerToggle;
 
+    //permisos
+    FirebaseFirestore db;
+    //hablar
+    private TextToSpeech tts;
+
+    //funcines bluetooth para escanear
+
+    private BroadcastReceiver mBluetoothReceiver;
+    private BluetoothLeScanner bluetoothLeScanner;
+    private Handler handler;
+
+    //permisos
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_PRIVILEGED
+    };
+    private static String[] PERMISSIONS_LOCATION = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_PRIVILEGED
+    };
+    private BluetoothAdapter mBluetoothAdapter;
+    private static final int REQUEST_BLUETOOTH_CONNECT = 2;
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (drawerToggle.onOptionsItemSelected(item)) {
@@ -64,6 +150,78 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        // Inicializamos la instancia de TextToSpeech
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.getDefault());
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(MainActivity.this, "Idioma no soportado", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Error al inicializar TextToSpeech", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //instancia de la interfaz
+        scanButton = findViewById(R.id.btnComenzar);
+        repetirButton = findViewById(R.id.btnRepetir);
+        masInfo=findViewById(R.id.btnInfo);
+        velocidad=findViewById(R.id.btnVelocidad);
+
+        //instancia de los bluetooth
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+        handler = new Handler();
+
+        //conexion a base de datos
+        db = FirebaseFirestore.getInstance();
+
+        db.collection("BEACONS")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        //permisos
+        checkPermissions();
+        //activacion de ubicacion
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Intent gpsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(gpsIntent);
+        }
+
+
+        //activacion de bluetooth
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            return;
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // Aquí se solicita el permiso de conexión Bluetooth si aún no se ha concedido.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.BLUETOOTH_CONNECT},
+                        REQUEST_BLUETOOTH_CONNECT);
+                return;
+            }
+            startActivity(enableBtIntent);
+        }
     }
 
     @Override
@@ -76,18 +234,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void checkPermissions(){
+        int permission1 = ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permission2 = ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN);
+        if (permission1 != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_STORAGE,
+                    1
+            );
+        } else if (permission2 != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    this,
+                    PERMISSIONS_LOCATION,
+                    1
+            );
+        }
+    }
+
     //metodo on click para los botones
     public void onClick(View view) {
         switch (view.getId()){
 
             case R.id.btnComenzar:
-                Toast.makeText(MainActivity.this, "boton comenzar", Toast.LENGTH_SHORT).show();
+                scaneo();
                 break;
             case R.id.btnRepetir:
-                Toast.makeText(MainActivity.this, "boton repetir", Toast.LENGTH_SHORT).show();
+                repetirObra();
                 break;
             case R.id.btnInfo:
-                Toast.makeText(MainActivity.this, "boton informacion", Toast.LENGTH_SHORT).show();
+                infoObra();
                 break;
             case R.id.btnMicro:
                 Toast.makeText(MainActivity.this, "boton micro", Toast.LENGTH_SHORT).show();
@@ -96,5 +273,178 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "boton velocidad", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+    //************************************************************************
+
+    //METODOOO ESCANEOO
+    public void scaneo(){
+        try {
+            //Activación de ubicación
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                Intent gpsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(gpsIntent);
+                return; // Salimos del método porque el usuario aún no ha habilitado la ubicación.
+            }
+
+            //Activación de Bluetooth
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter == null) {
+                // Device does not support Bluetooth
+                return;
+            }
+
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // Aquí se solicita el permiso de conexión Bluetooth si aún no se ha concedido.
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.BLUETOOTH_CONNECT},
+                            REQUEST_BLUETOOTH_CONNECT);
+                    return;
+                }
+                startActivity(enableBtIntent);
+                registerBluetoothReceiver(); // Registra el BroadcastReceiver para esperar la activación del Bluetooth.
+                return; // Salimos del método porque el usuario aún no ha habilitado Bluetooth.
+            }
+
+            // Si llegamos aquí, es porque la ubicación y el Bluetooth están habilitados. Podemos empezar a escanear.
+            startScanning();
+        } catch (Exception e) {
+            // Manejamos la excepción aquí y podemos hacer cualquier cosa, como mostrar un mensaje de error.
+            Log.e(TAG, "Error: " + e.getMessage());
+            Toast.makeText(this, "Configurando." + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void registerBluetoothReceiver() {
+        // Registra el BroadcastReceiver para detectar cuando el Bluetooth esté habilitado.
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        mBluetoothReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                final String action = intent.getAction();
+                if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                    final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    if (state == BluetoothAdapter.STATE_ON) {
+                        startScanning();
+                    }
+                }
+            }
+        };
+        registerReceiver(mBluetoothReceiver, filter);
+    }
+
+    public void infoObra(){
+        if(descripcion!=null && descripcion!=""){
+            tts.speak(descripcion, TextToSpeech.QUEUE_ADD, null, null);
+        }else{
+            tts.speak("Debes comenzar el trayecto.", TextToSpeech.QUEUE_ADD, null, null);
+        }
+    }
+    public void repetirObra(){
+        if(titulo!=null && titulo!=""){
+            tts.speak(titulo, TextToSpeech.QUEUE_ADD, null, null);
+        }else{
+            tts.speak("Debes comenzar el trayecto.", TextToSpeech.QUEUE_ADD, null, null);
+        }
+    }
+
+
+    //*************************************************************************
+
+    private void startScanning() {
+        if (!scanning) {
+            // Iniciar el escaneo
+            scanning = true;
+            scanButton.setText("Buscando");
+            text = "Comenzamos.";
+            tts.speak(text, TextToSpeech.QUEUE_ADD, null, null);
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //stopScanning();
+                }
+            }, 0); // Escanear durante 5 segundo
+            bluetoothLeScanner.startScan(new BeaconScanCallback());
+        } else {
+            // Detener el escaneo
+            stopScanning();
+        }
+
+    }
+
+    private void stopScanning() {
+        if (scanning) {
+            scanning = false;
+            scanButton.setText("Comenzar");
+            text = "Viaje terminado.";
+            titulo="";
+            descripcion="";
+            tts.speak(text, TextToSpeech.QUEUE_ADD, null, null);
+            leidos.clear();
+            bluetoothLeScanner.stopScan(new BeaconScanCallback());
+        }
+    }
+
+
+    private class BeaconScanCallback extends ScanCallback {
+        private List<ScanFilter> filters;
+
+        public BeaconScanCallback(List<ScanFilter> filters) {
+            this.filters = filters;
+        }
+
+        public BeaconScanCallback() {
+        }
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            BluetoothDevice device = result.getDevice();
+            ScanRecord scanRecord = result.getScanRecord();
+            String nombre=""+result.getDevice().getName();
+            String cod=result.getDevice().getAddress();
+            int distancia =result.getRssi();
+
+            if(scanButton.getText().equals("Buscando")) {
+                if (!leidos.contains(cod)) {
+                //Toast.makeText(MainActivity.this, "Beacon descubierto: " + result.getDevice().getAddress() + " " + result.getDevice().getName() + " " + result.getRssi(), Toast.LENGTH_SHORT).show();
+                Log.d("MainActivity", "Beacon descubierto: " + result.getDevice().getAddress() + " " + result.getDevice().getName() + " " + result.getRssi());
+                // Obtener los datos del documento "cod" de la colección "BEACONS" de Firebase Firestore
+                db.collection("BEACONS")
+                        .document(cod)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    // Obtener los valores de los campos "titulo" y "descripcion"
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        titulo= document.getString("titulo");
+                                        descripcion = document.getString("descripcion");
+                                        disString=document.getString("rssi");
+                                        conversionRSSI=Integer.parseInt(disString);
+
+                                        if(distancia>=conversionRSSI && !leidos.contains(cod)){
+                                            leidos.add(cod);
+                                            tts.speak(titulo, TextToSpeech.QUEUE_ADD, null, null);
+                                        }
+
+
+                                        // Hacer lo que sea necesario con los valores obtenidos
+                                    } else {
+                                        Log.d(TAG, "El documento no existe");
+                                    }
+                                } else {
+                                    Log.d(TAG, "Error al obtener los datos del documento", task.getException());
+                                }
+                            }
+                        });
+            }}
+
+
+        }
+
     }
 }
